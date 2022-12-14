3049003738c07270db1d9704dc1c2289efc1883b
Roman Lemus <roman09071999@gmail.com>
2022-12-14 14:26:03 -0600

    First Commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { FaShoppingCart, FaBars, FaUserAlt } from "react-icons/fa";
+import { SiReacttable } from "react-icons/si";
+import { IoMdClose } from "react-icons/io";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  const [open, setOpen] = useState(false);
+  return <></>;
+};
+
+export default NavBar;
---
+++ src/components/Router.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "../pages/NotFound";
+import Account from "../pages/Account";
+import Checkout from "../pages/Checkout";
+import Home from "../pages/Home";
+import SignUp from "../pages/SignUp";
+import Cart from "../pages/Cart";
+import NavBar from "./NavBar";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
